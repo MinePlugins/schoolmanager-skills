888951490e82251751b6ef979ea4ae0478cb98ee
Quentin Cournut <quentin.cournut@siview.fr>
2022-02-12 13:19:15 +0100

    Added dialog and CFA insta API to get planning7

--- __init__.py
+++ __init__.py
@@ -12,36 +12,45 @@ utc=pytz.UTC
 
 
 
-def get_cours(promo, date):
-    r = requests.get(f'https://schoolmanager.cfa-insta.fr:3030/api/promotion/{promo}/planning', verify=False)
-    data = r.json()
-    now = datetime.datetime.now()
-    cours = None
-    for i in data['response']:
-        
-        date = parser.parse(i['module_date'])
-        hourf = parser.parse(i['time_start'])
-        houre = parser.parse(i['time_end'])
-        if (date.month == now.month and date.day == now.day and date.year == now.year):
-            if hourf <= date <= houre:
-                cours = i['name']
 
-    if cours is not None:
-        return cours
-    else: 
-        return "Tu nas aucun cours "
 class Schoolmanager(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('schoolmanager.intent')
     def handle_schoolmanager(self, message):
-        date = self.get_response('Donne moi un date ?')
+        date = self.get_response('Donne moi une date ?')
         date = dateparser.parse(date)
+        self.log.info(f'Date parser :{date}')
         promo = self.get_response('quel.est.ta.promo ?')
 
-        self.speak_dialog(get_cours(promo, date))
+        self.speak_dialog(self.get_cours(promo, date))
 
+    def get_cours(self, promo, dateuser):
+        r = requests.get(f'https://schoolmanager.cfa-insta.fr:3030/api/promotion/{promo}/planning', verify=False)
+        data = r.json()
+        now = datetime.datetime.now()
+        cours = None
+        for i in data['response']:
+            
+            date = parser.parse(i['module_date'])
+            hourf = parser.parse(i['time_start'])
+            houre = parser.parse(i['time_end'])
+            hourf = hourf.replace(day=date.day,month=date.month,year=date.year)
+            houre = houre.replace(day=date.day,month=date.month,year=date.year)
+            if (date.month == dateuser.month and date.day == dateuser.day and date.year == dateuser.year):
+                dateuser = dateuser.replace(hour=9, minute=5)
+                self.log.info(f'Date == :{dateuser}')
+                self.log.info(f'Date == :{hourf}')
+                self.log.info(f'Date == :{houre}')
+
+                if hourf <= dateuser <= houre:
+                    cours = i['name']
+
+        if cours is not None:
+            return cours
+        else: 
+            return "Tu n'as aucun cours "
 
 def create_skill():
     return Schoolmanager()
